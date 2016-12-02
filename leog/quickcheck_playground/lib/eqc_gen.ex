236112e9e6650325b56c309b09fd28e0c653cd64
Marius Butuc <marius.butuc@gmail.com>
2016-12-01 23:50:02 -0500

    Generating arbitrarily nested lists

--- leog/quickcheck_playground/lib/eqc_gen.ex
+++ leog/quickcheck_playground/lib/eqc_gen.ex
@@ -1,6 +1,24 @@
 defmodule EQCGen do
   use EQC.ExUnit
 
+  def nested_list(generator) do
+    sized size do
+      nested_list(size, generator)
+    end
+  end
+
+  defp nested_list(0, _generator) do
+    []
+  end
+  defp nested_list(size, generator) do
+    lazy do
+      oneof [
+        [generator | nested_list(size-1, generator)],
+        [nested_list(size-1, generator)]
+      ]
+    end
+  end
+
   def string_with_fixed_length do
     let len <- choose(1, 10) do
       vector(len, frequency([
