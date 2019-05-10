c26b0e64c4a33f3b82a8f10f10329bfcc053d8bc
Marius Butuc <marius.butuc@gmail.com>
2019-05-09 22:52:41 -0400

    FizzBuzz, version 2
    For the "clever" solution, build up a string containing the word or 
    words to output and print either this word or the number if there is 
    no word, potentially by making good use of the || operator.

--- eloquent-javascript/02_program_structure/02_fizz_buzz.js
+++ eloquent-javascript/02_program_structure/02_fizz_buzz.js
@@ -9,19 +9,25 @@
  * When you have that working, modify your program to print "FizzBuzz" for
  * numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz"
  * for numbers divisible by only one of those).
+ *
+ * For the "clever" solution, build up a string containing the word or words to
+ * output and print either this word or the number if there is no word,
+ * potentially by making good use of the || operator.
  */
 
 const fizz = "Fizz";
 const buzz = "Buzz";
 
 for (let counter = 1; counter <= 100; counter++) {
+  let words = "";
+
   if (counter % 3 === 0 && counter % 5 === 0) {
-    console.log(fizz + buzz);
+    words = words + fizz + buzz;
   } else if (counter % 3 === 0) {
-    console.log(fizz);
+    words = words + fizz;
   } else if (counter % 5 === 0) {
-    console.log(buzz);
-  } else {
-    console.log(counter)
+    words = words + buzz;
   }
+
+  console.log(words || counter);
 }
