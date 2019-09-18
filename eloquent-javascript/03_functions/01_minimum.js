2e9cb1d407e350b2d3852cfed796db43fdbb6ba8
Marius Butuc <marius.butuc@gmail.com>
2019-09-18 07:42:08 -0400

    Eloquent JavaScript » Chapter 03: Functions
    * the 3 different ways of creating functions
    * scope: global, local, block, nested, lexical
    * the call stack
    * optional arguments
    * closures: function values contain both the code and the environment
                in which they were created
    * recursion: human-friendliness (elegance) versus
                 machine-friendliness (speed)
    * functions and side effects: pure functions and computing speed

---
+++ eloquent-javascript/03_functions/01_minimum.js
@@ -0,0 +1,22 @@
+/**
+ * Minimum
+ *
+ * The previous chapter introduced the standard function `Math.min` that returns
+ * its smallest argument. We can build something like that now.
+ * Write a function `min` that takes two arguments and returns their minimum.
+ */
+
+/**
+ * Calculate the minimum between two numbers
+ *
+ * @param {number} a The first number
+ * @param {number} b The second number
+ */
+const min = (a, b) => {
+    // Your code here.
+}
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
