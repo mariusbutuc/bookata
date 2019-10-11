c6b813b9c739a9ee23e607403aa9b88f16ba0ad3
Marius Butuc <marius.butuc@gmail.com>
2019-10-11 02:09:52 -0400

    Implement reverseArray as a pure function
    Produce a new array.

--- eloquent-javascript/04_data_structures/02_reverse_array.js
+++ eloquent-javascript/04_data_structures/02_reverse_array.js
@@ -22,7 +22,13 @@
  *    original `array`, but in the inverse order.
  */
 function reverseArray(array) {
-  // Your code here.
+  reversedArray = [];
+
+  for (let element of array) {
+    reversedArray.unshift(element);
+  }
+
+  return reversedArray;
 }
 
 /**
@@ -44,5 +50,5 @@ console.log(reverseArray(["A", "B", "C"]));
 
 let arrayValue = [1, 2, 3, 4, 5];
 reverseArrayInPlace(arrayValue);
-console.log(arrayValue);
+// console.log(arrayValue);
 // → [5, 4, 3, 2, 1]
