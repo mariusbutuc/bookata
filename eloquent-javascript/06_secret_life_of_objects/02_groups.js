cc719f90a79c2f017043f6bfaa5af1656b028281
Marius Butuc <marius.butuc@lever.co>
2020-01-22 23:28:22 -0500

    Write a Group class
    First take, before looking at the hints.

---
+++ eloquent-javascript/06_secret_life_of_objects/02_groups.js
@@ -0,0 +1,67 @@
+/**
+ * Groups
+ *
+ * The standard JavaScript environment provides another data structure called
+ * `Set`. Like an instance of `Map`, a set holds a collection of values.
+ * Unlike `Map`, it does not associate other values with those—it just tracks
+ * which values are part of the set. A value can be part of a set only
+ * once—adding it again doesn’t have any effect.
+ *
+ * Write a class called `Group` (since `Set` is already taken). Like `Set`, it
+ * has `add`, `delete`, and `has` methods. Its constructor creates an empty
+ * group, `add` adds a value to the group (but only if it isn’t already a
+ * member), `delete` removes its argument from the group (if it was a member),
+ * and `has` returns a Boolean value indicating whether its argument is a member
+ * of the group.
+ *
+ * Use the `===` operator, or something equivalent such as `indexOf`, to
+ * determine whether two values are the same.
+ *
+ * Give the class a static `from` method that takes an iterable object as
+ * argument and creates a group that contains all the values produced by
+ * iterating over it.
+ */
+
+class Group {
+  constructor() {
+    this.values = [];
+  }
+
+  add(element) {
+    if (this.values.indexOf(element) === -1) {
+      this.values.push(element)
+    }
+  }
+
+  has(element) {
+    return this.values.indexOf(element) >= 0
+  }
+
+  delete(element) {
+    if (this.has(element)) {
+      let elementIndex = this.values.indexOf(element);
+      this.values.splice(elementIndex, 1);
+    }
+  }
+
+  static from(iterable) {
+    let group = new Group();
+
+    for (let element of iterable) {
+      group.add(element);
+    }
+
+    return group;
+  }
+
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+// → false
