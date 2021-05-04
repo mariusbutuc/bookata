b2afea613f6df4d0f46ba99189c5d15c4f320937
Marius Butuc <mariusbutuc+atlist@gmail.com>
2021-05-03 21:28:41 -0400

    Begin interacting with the authentication service
    + be more explicit in dependency declaration
    + fix Prettier and ESLint integration in VSCode

--- full-stack-serverless/01-full-stack-dev-in-serverless-era/amplify-app/bootstrap-01-yarn2.sh
+++ full-stack-serverless/01-full-stack-dev-in-serverless-era/amplify-app/bootstrap-01-yarn2.sh
@@ -9,4 +9,7 @@ yarn dlx @yarnpkg/pnpify --sdk vscode
 yarn add --dev eslint
 yarn add --dev prettier
 
+# https://github.com/facebook/create-react-app/issues/10463#issuecomment-813702443
+yarn add eslint-config-react-app
+
 yarn dlx @yarnpkg/doctor
