8346bfe2064a13edae9f08a900c960e695b56541
Marius Butuc <mariusbutuc+atlist@gmail.com>
2021-05-08 14:35:22 -0400

    Enable the user to specify `start` and `limit`
    * Create additional state to hold user input
    * Create function to allow users to update input values
    * Update `fetchCoins` to use the new properties
    * Add input fields and button to UI

--- full-stack-serverless/02-getting-started/amplify-react-app/src/App.js
+++ full-stack-serverless/02-getting-started/amplify-react-app/src/App.js
@@ -9,15 +9,28 @@ import './App.css';
 function App() {
 	// Create `coins` variable and set to empty array
 	const [coins, updateCoins] = useState([]);
+	// Create additional state to hold user input for `limit` and `start` properties
+	const [input, updateInput] = useState({ limit: 5, start: 0 });
 
+	function updateInputValues(type, value) {
+		updateInput({ ...input, [type]: value });
+	}
+
+	// Update to use `limit` and `start` properties
 	async function fetchCoins() {
-		const data = await API.get('cryptoapi', '/coins');
+		const { limit, start } = input;
+		const data = await API.get(
+			'cryptoapi',
+			`/coins?limit=${limit}&start=${start}`
+		);
 		updateCoins(data.coins);
 	}
 
 	// Call fetchCoins function when component loads
 	useEffect(() => {
 		fetchCoins();
+		// FIXME: Understand the dependency array suggestion below and the implications of ignoring it.
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	return (
@@ -30,6 +43,17 @@ function App() {
 					<h5>${coin.price_usd}</h5>
 				</div>
 			))}
+
+			<input
+				onChange={(e) => updateInputValues('limit', e.target.value)}
+				placeholder="limit"
+			/>
+			<input
+				onChange={(e) => updateInputValues('start', e.target.value)}
+				placeholder="start"
+			/>
+
+			<button onClick={fetchCoins}>Fetch Coins</button>
 		</div>
 	);
 }
