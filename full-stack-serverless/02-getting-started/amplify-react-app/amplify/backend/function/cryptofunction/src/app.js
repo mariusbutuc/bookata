36a7f3d290354f18911b245cd62a531144ed516d
Marius Butuc <mariusbutuc+atlist@gmail.com>
2021-05-08 14:09:15 -0400

    Update serverless function to call external API
    * Use `axios` to interact with the HTTP endpoint in the Lambda function
    * Pass `start` and `limit` params to filter API response
    * Get params from `req.apiGateway.event.queryStringParameters`

--- full-stack-serverless/02-getting-started/amplify-react-app/amplify/backend/function/cryptofunction/src/app.js
+++ full-stack-serverless/02-getting-started/amplify-react-app/amplify/backend/function/cryptofunction/src/app.js
@@ -10,6 +10,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
 
+const axios = require('axios');
+
 // declare a new express app
 var app = express();
 app.use(bodyParser.json());
@@ -22,70 +24,26 @@ app.use(function (req, res, next) {
 	next();
 });
 
-/**********************
- * Example get method *
- **********************/
-
 app.get('/coins', function (req, res) {
-	const coins = [
-		{ name: 'Bitcoin', symbol: 'BTC', price_usd: '10000' },
-		{ name: 'Ethereum', symbol: 'ETH', price_usd: '400' },
-		{ name: 'Litecoin', symbol: 'LTC', price_usd: '150' },
-	];
+	// Define base url
+	let apiUrl = 'https://api.coinlore.com/api/tickers?start=0&limit=10';
 
-	res.json({ coins });
-});
+	// Mutate base url to include query parameters, if any
+	if (req.apiGateway && req.apiGateway.event.queryStringParameters) {
+		const {
+			start = 0,
+			limit = 10,
+		} = req.apiGateway.event.queryStringParameters;
+		apiUrl = `https://api.coinlore.com/api/tickers?start=${start}&limit=${limit}`;
+	}
 
-app.get('/item', function (req, res) {
-	// Add your code here
-	res.json({ success: 'get call succeed!', url: req.url });
-});
-
-app.get('/item/*', function (req, res) {
-	// Add your code here
-	res.json({ success: 'get call succeed!', url: req.url });
-});
-
-/****************************
- * Example post method *
- ****************************/
-
-app.post('/item', function (req, res) {
-	// Add your code here
-	res.json({ success: 'post call succeed!', url: req.url, body: req.body });
-});
-
-app.post('/item/*', function (req, res) {
-	// Add your code here
-	res.json({ success: 'post call succeed!', url: req.url, body: req.body });
-});
-
-/****************************
- * Example put method *
- ****************************/
-
-app.put('/item', function (req, res) {
-	// Add your code here
-	res.json({ success: 'put call succeed!', url: req.url, body: req.body });
-});
-
-app.put('/item/*', function (req, res) {
-	// Add your code here
-	res.json({ success: 'put call succeed!', url: req.url, body: req.body });
-});
-
-/****************************
- * Example delete method *
- ****************************/
-
-app.delete('/item', function (req, res) {
-	// Add your code here
-	res.json({ success: 'delete call succeed!', url: req.url });
-});
-
-app.delete('/item/*', function (req, res) {
-	// Add your code here
-	res.json({ success: 'delete call succeed!', url: req.url });
+	// Call API & return response
+	axios
+		.get(apiUrl)
+		.then((response) => {
+			res.json({ coins: response.data.data });
+		})
+		.catch((err) => res.json({ error: err }));
 });
 
 app.listen(3000, function () {
