433296a7220828117b026f9cdfe0d5830f9018da
Marius Butuc <mariusbutuc+atlist@gmail.com>
2021-05-08 13:37:08 -0400

    Use `API` category to fetch data and render it
    Fetch data from
    1. the API endpoint `/coins` wrapping
    2. the serverless/Lambda function
    
    Render it in React.

--- full-stack-serverless/02-getting-started/amplify-react-app/src/App.js
+++ full-stack-serverless/02-getting-started/amplify-react-app/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+// React hooks
+import React, { useState, useEffect } from 'react';
+
+// API category from AWS Amplify
+import { API } from 'aws-amplify';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// Create `coins` variable and set to empty array
+	const [coins, updateCoins] = useState([]);
+
+	async function fetchCoins() {
+		const data = await API.get('cryptoapi', '/coins');
+		updateCoins(data.coins);
+	}
+
+	// Call fetchCoins function when component loads
+	useEffect(() => {
+		fetchCoins();
+	}, []);
+
+	return (
+		<div className="App">
+			{coins.map((coin, index) => (
+				<div key={index}>
+					<h2>
+						{coin.name} - {coin.symbol}
+					</h2>
+					<h5>${coin.price_usd}</h5>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default App;
