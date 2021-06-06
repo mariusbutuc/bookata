bcab1e6bbcea5eb9c7af6a1a93b1e9de64bda991
Marius Butuc <mariusbutuc+atlist@gmail.com>
2021-06-06 18:40:11 -0400

    Add custom logic to resize the image
    + Fetch the image from S3
    + For images with a width > 1_000px
      + Use the Sharp library to resize to 1_000px wide
      + Save back to S3
    + no-op otherwise

--- full-stack-serverless/06-serverless-fun-1/amplify/backend/function/S3Trigger520bec93/src/index.js
+++ full-stack-serverless/06-serverless-fun-1/amplify/backend/function/S3Trigger520bec93/src/index.js
@@ -1,8 +1,34 @@
+// Import the `sharp` library
+const sharp = require('sharp');
+const aws = require('aws-sdk');
+const s3 = new aws.s3();
+
 // eslint-disable-next-line
-exports.handler = async function (event) {
-  console.log('Received S3 event:', JSON.stringify(event, null, 2));
-  // Get the object from the event and show its content type
-  const bucket = event.Records[0].s3.bucket.name; //eslint-disable-line
-  const key = event.Records[0].s3.object.key; //eslint-disable-line
-  console.log(`Bucket: ${bucket}`, `Key: ${key}`);
+exports.handler = async function (event, context) {
+  // If the event type is `delete`, return from the function
+  if (event.Records[0].eventName === 'ObjectRemoved:Delete') return;
+
+  // Next, we get the bucket name and the key from the event
+  const BUCKET = event.Records[0].s3.bucket.name; //eslint-disable-line
+  const KEY = event.Records[0].s3.bucket.key; //eslint-disable-line
+
+  try {
+    // Fetch the image data from S3
+    let image = await s3.getObject({ Bucket: BUCKET, Key: KEY }).promise();
+    image = await sharp(image.Body);
+
+    // Get the metadata from the image, including the width and height
+    const metadata = await image.metadata();
+    if (metadata.width > 1000) {
+      const resizedImage = await image.resize({ width: 1000 }).toBuffer();
+      await s3
+        .putObject({ Bucket: BUCKET, Body: resizedImage, Key: KEY })
+        .promise();
+      return;
+    } else {
+      return;
+    }
+  } catch (err) {
+    context.fail(`Error getting files: ${err}`);
+  }
 };
