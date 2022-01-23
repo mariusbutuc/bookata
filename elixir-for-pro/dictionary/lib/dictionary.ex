d73ca7ab09c5d58e7c30c4428cd51bf08791c5ae
Marius Butuc <marius.butuc@gmail.com>
2022-01-23 00:31:39 -0500

    Refactor the Dictionary module » more idiomatic
    + thinking in functions
    + decoupling state (always immutable) from behaviour
    + the pipeline operator
    + (compile time) module attributes
    
    Main tools
    + functional composition (pipelines)
    + pattern matching

--- elixir-for-pro/dictionary/lib/dictionary.ex
+++ elixir-for-pro/dictionary/lib/dictionary.ex
@@ -1,15 +1,15 @@
 defmodule Dictionary do
   @doc """
-  Turn [the provided the word list file][1] into a list of words.
+  Compile the word list file [1] into a list of words.
 
     [1]: https://coding-gnome.com/courses/e4p2/assets/words.txt
   """
-  def word_list do
-    words = File.read!("assets/words.txt")
-    String.split(words, ~r{\n}, trim: true)
-  end
+  @word_list "assets/words.txt"
+             |> File.read!()
+             |> String.split(~r{\n}, trim: true)
 
   def random_word do
-    Enum.random(word_list())
+    @word_list
+    |> Enum.random()
   end
 end
