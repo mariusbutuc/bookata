7ad27c08d697617c07be79b5e0dd9a2be9797559
Marius Butuc <marius.butuc@gmail.com>
2022-01-22 23:49:42 -0500

    Add Dictionary module—the MVP version

--- elixir-for-pro/dictionary/lib/dictionary.ex
+++ elixir-for-pro/dictionary/lib/dictionary.ex
@@ -1,18 +1,15 @@
 defmodule Dictionary do
-  @moduledoc """
-  Documentation for `Dictionary`.
-  """
-
   @doc """
-  Hello world.
+  Turn [the provided the word list file][1] into a list of words.
 
-  ## Examples
-
-      iex> Dictionary.hello()
-      :world
-
+    [1]: https://coding-gnome.com/courses/e4p2/assets/words.txt
   """
-  def hello do
-    :world
+  def word_list do
+    words = File.read!("assets/words.txt")
+    String.split(words, ~r{\n}, trim: true)
+  end
+
+  def random_word do
+    Enum.random(word_list())
   end
 end
