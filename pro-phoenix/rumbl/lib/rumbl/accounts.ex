a1548f1037382ea23600365a76758a57c775abfa
Marius Butuc <marius.butuc@gmail.com>
2022-05-30 13:39:06 -0400

    Building Forms
    + Add `UserController` action to set up new user account struct.
    + Add User `changeset/2` alongside schema (casting + validations).
    + Publicly expose `change_user/1` from Accounts context.
    + Publish `:new` and `:create` routes using the `resources` macro.
    + Create new template with form using `form_for/3`.

--- pro-phoenix/rumbl/lib/rumbl/accounts.ex
+++ pro-phoenix/rumbl/lib/rumbl/accounts.ex
@@ -11,7 +11,12 @@ defmodule Rumbl.Accounts do
   end
 
   def get_user(id), do: Repo.get(User, id)
+
   def get_user!(id), do: Repo.get!(User, id)
 
   def get_user_by(params), do: Repo.get_by(User, params)
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
 end
