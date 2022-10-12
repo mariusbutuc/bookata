2e0018f7dcca02ce3f30b2b74f2bc5271a97a870
Marius Butuc <marius.butuc@gmail.com>
2022-10-12 18:11:47 -0400

    Inherit latest Nix & Docker dependency declaration
    https://github.com/mariusbutuc/shell.nixes

---
+++ prog-phx-liveview/README.md
@@ -0,0 +1,32 @@
+# Programming Phoenix LiveView [↗️]
+
+## Plan
+
+0. [x] Introduction
+
+### Part 1. Code Generation
+
+1.  [x] Get to Know LiveView
+2.  [ ] Phoenix & Authentication
+3.  [ ] Generators: Contexts & Schemas
+4.  [ ] Generators: Live Views & Templates
+
+### Part 2. LiveView Composition
+
+5.  [ ] Forms & Changesets
+6.  [ ] Function Components
+7.  [ ] Live components
+
+### Part 3. Exctend LiveView
+
+8.  [ ] Build an Interactive Dashboard
+9.  [ ] Build a Distributed Dashboard
+10. [ ] Test you Live Views
+
+### Part 4. Graphics & Custom Code Organization
+
+11. [ ] Build the Game Core
+12. [ ] Render Graphics with SVG
+13. [ ] Establish Boundaries & APIs
+
+    [↗️]: https://pragprog.com/titles/liveview/programming-phoenix-liveview/
---
+++ prog-phx-liveview/shell.nix
@@ -0,0 +1,48 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+
+let
+  basePackages = [
+    bat
+    beam.packages.erlangR25.elixir_1_14
+    git
+    tig
+  ];
+
+  inputs = basePackages
+    ++ lib.optionals stdenv.isLinux [
+      # https://hexdocs.pm/phoenix/installation.html#inotify-tools-for-linux-users
+      inotify-tools
+    ]
+    ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+      CoreFoundation
+      CoreServices
+    ]);
+
+  hooks = ''
+    export HEX_HOME=$PWD/.nix-hex
+    export MIX_HOME=$PWD/.nix-mix
+    export PHX_VERSION="1.6.13"
+
+    mkdir -p $MIX_HOME $HEX_HOME
+
+    # leverage hex from Nixpkgs
+    export MIX_PATH="${beam.packages.erlang.hex}/lib/erlang/lib/hex/ebin"
+
+    export PATH=$MIX_HOME/bin:$HEX_HOME/bin:$PATH
+    export LANG=C.UTF-8
+
+    if ! mix phx.new --version; then
+      mix archive.install hex phx_new $PHX_VERSION --force
+    fi
+
+    # persist iex shell history
+    export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_file_bytes 1024000"
+  '';
+in
+
+mkShell {
+  buildInputs = inputs;
+  shellHook = hooks;
+}
